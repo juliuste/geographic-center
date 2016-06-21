14b8ea0a8362b4257de1df412f37cd6e341d74a6
Julius Te <mail@juliuste.de>
2016-06-21 17:34:09 +0200

    added license, test cleanup

--- test.js
+++ test.js
@@ -4,16 +4,15 @@
 const assert = require('assert')
 const gc = require('./index.js')
 
-
-let point = {lat: 13.4, lon: 52.3}
-
-let round = (obj) => {
-	for(let i in obj) obj[i] = Math.round(obj[i]*10e5)/10e5
-	return obj
-}
+const round = (value) => Math.round(value*10e5)/10e5
 
 /*assert.deepEqual(round(gc.utm2wgs(gc.wgs2utm(point, point),point)), round(point))
 
 assert.deepEqual(round(gc.getRefPoint([point])), round(point))*/
 
-assert.deepEqual(round(gc([point])), round(point))
+let point = {lat: 13.4, lon: 52.3}
+
+let center = gc([point])
+
+assert.deepEqual(round(point.lat), round(center.lat))
+assert.deepEqual(round(point.lon), round(center.lon))
