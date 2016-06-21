0840f1dfdb8666268d41b80f4c4c6ef331ab4239
Julius Te <mail@juliuste.de>
2016-06-21 17:41:28 +0200

    bugfix.

--- test.js
+++ test.js
@@ -6,7 +6,7 @@ const gc = require('./index.js')
 
 let point = {lat: 13.4, lon: 52.3}
 
-center = gc([point])
+let center = gc([point])
 
 assert.deepEqual(Math.round(point.lat*10e5)/10e5, Math.round(center.lat*10e5)/10e5)
 assert.deepEqual(Math.round(point.lon*10e5)/10e5, Math.round(center.lon*10e5)/10e5)
